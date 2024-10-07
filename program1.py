1550e33641bd99d60939cff66a4a3258bd21eb03
Archana Raj <archanashah380@gmail.com>
2024-10-07 14:06:19 +0530

    Done

--- program1.py
+++ program1.py
@@ -4,7 +4,16 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+        stack = []
+        mapping = {")": "(", "}": "{", "]": "["}
+
+        for char in s:
+            if char in mapping.values():
+                stack.append(char)
+            elif char in mapping:
+                if not stack or mapping[char] != stack.pop():
+                    return False
+        return not stack
 
 
 
